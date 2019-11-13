ce55baaf70455a1b2b4e9bb2bff18c080b9e3c87
Chalant <strikerklm96@gmail.com>
2019-11-13 16:05:11 -0600

    move stuff

--- Server Config/Super Server/inside DST Dedicated install/mods/workshop-456028339/modmain.lua
+++
@@ -1,8 +0,0 @@
-STRINGS = GLOBAL.STRINGS
-RECIPETABS = GLOBAL.RECIPETABS
-Recipe = GLOBAL.Recipe
-Ingredient = GLOBAL.Ingredient
-TECH = GLOBAL.TECH
-
-local living_log = GLOBAL.Recipe("livinglog", {Ingredient("log", 1),Ingredient("nightmarefuel", 1) }, RECIPETABS.REFINE, TECH.MAGIC_TWO )
-STRINGS.RECIPE_DESC.LIVINGLOG = "Shove your inner demons into this wood."
--- Server Config/Super Server/launch_preconfigured_servers.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-set SteamAppId=322330
-set SteamGameId=322330
-
-cd ..
-start "Don't Starve Together Overworld" /D "%~dp0.." "%~dp0..\dontstarve_dedicated_server_nullrenderer.exe" -conf_dir DoNotStarveTogether_EasyConfigOverworld -console
-start "Don't Starve Together Caves"     /D "%~dp0.." "%~dp0..\dontstarve_dedicated_server_nullrenderer.exe" -conf_dir DoNotStarveTogether_EasyConfigCaves -console
-
